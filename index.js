7e3775cb3cdcdddf117e4d60aae1218543700084
thgml723411-byte <thgml723411@gmail.com>
2026-06-02 14:34:19 +0900

    nav수정완료

--- index.js
+++ index.js
@@ -1,5 +1,47 @@
-//Observer
+let gnb = document.querySelector(".gnb")
+let nav = document.querySelector(".nav")
+let ham = document.querySelector('.ham')
+let fullmenu = document.querySelector('.fullmenu')
+let closebtn = document.querySelector('.closebtn')
+function onpage(){
+}
+// let ses = document.querySelectorAll('.wrap>section')
+
+//메뉴상단
+if(gnb && nav){
+    gnb.addEventListener("mouseenter", function(){
+        nav.classList.add("on")
+    })
+    gnb.addEventListener("mouseleave", function(){
+        nav.classList.remove("on")
+    })
+}
+
+//햄버거바
+if(ham && fullmenu){
+    ham.addEventListener('click',function(){
+        fullmenu.classList.add('on')
+    })
+}
+if(closebtn && fullmenu){
+    closebtn.addEventListener('click',function(){
+        fullmenu.classList.remove('on')
+    })
+}
+
+//섹션
+//     function showpage(num){
+//         ses.forEach(function(item){
+//            item.classList.remove('on')
+//         })
+//         ses[num].classList.add('on')
+//         ham.classList.remove('on')
+//         fullmenu.classList.remove('on')
+// }
+
+//옵저버
 let items = document.querySelectorAll(".scale1, .up2, .up3, .up4, .up5, .scale6, .up7 ")
+if(items.length){
 const observer = new IntersectionObserver(function(entries){
     entries.forEach(function(entry){
             if(entry.isIntersecting){
@@ -14,3 +56,4 @@ const observer = new IntersectionObserver(function(entries){
 items.forEach(function(box){
   observer.observe(box)
 })
+}
